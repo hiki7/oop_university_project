ea944c33c2d515dffa8c84453c5dd4fb8bd1f7e8
Yerzhan Rsaliyev <rsaliyev03@gmail.com>
2023-12-13 20:21:29 +0500

    fixed the spacing in the ResearchProject class

--- New Folder/src/ResearchObjects/ResearchProject.java
+++ New Folder/src/ResearchObjects/ResearchProject.java
@@ -8,60 +8,60 @@ public class ResearchProject {
      
     }
 
- public ResearchProject(String topic, List<ResearchPaper> publishedPapers, List<Researcher> projectParticipants) {
-  super();
-  this.topic = topic;
-  this.publishedPapers = publishedPapers;
-  this.projectParticipants = projectParticipants;
- }
+    public ResearchProject(String topic, List<ResearchPaper> publishedPapers, List<Researcher> projectParticipants) {
+    	super();
+    	this.topic = topic;
+    	this.publishedPapers = publishedPapers;
+    	this.projectParticipants = projectParticipants;
+    }
 
- public String getTopic() {
-  return topic;
- }
+    public String getTopic() {
+    	return topic;
+    }
 
- public void setTopic(String topic) {
-  this.topic = topic;
- }
+    public void setTopic(String topic) {
+    	this.topic = topic;
+    }
 
- public List<ResearchPaper> getPublishedPapers() {
-  return publishedPapers;
- }
+    public List<ResearchPaper> getPublishedPapers() {
+    	return publishedPapers;
+    }
 
- public void setPublishedPapers(List<ResearchPaper> publishedPapers) {
-  this.publishedPapers = publishedPapers;
- }
+    public void setPublishedPapers(List<ResearchPaper> publishedPapers) {
+    	this.publishedPapers = publishedPapers;
+    }
 
- public List<Researcher> getProjectParticipants() {
-  return projectParticipants;
- }
+    public List<Researcher> getProjectParticipants() {
+    	return projectParticipants;
+    }
 
- public void setProjectParticipants(List<Researcher> projectParticipants) {
-  this.projectParticipants = projectParticipants;
- }
+    public void setProjectParticipants(List<Researcher> projectParticipants) {
+    	this.projectParticipants = projectParticipants;
+    }
  
- @Override
- public int hashCode() {
-  return Objects.hash(projectParticipants, publishedPapers, topic);
+    @Override
+    public int hashCode() {
+    	return Objects.hash(projectParticipants, publishedPapers, topic);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+    	if (this == obj)
+    		return true;
+    	if (obj == null)
+    		return false;
+    	if (getClass() != obj.getClass())
+    		return false;
+    	ResearchProject other = (ResearchProject) obj;
+    	return Objects.equals(projectParticipants, other.projectParticipants)
+    			&& Objects.equals(publishedPapers, other.publishedPapers) && Objects.equals(topic, other.topic);
  }
 
- @Override
- public boolean equals(Object obj) {
-  if (this == obj)
-   return true;
-  if (obj == null)
-   return false;
-  if (getClass() != obj.getClass())
-   return false;
-  ResearchProject other = (ResearchProject) obj;
-  return Objects.equals(projectParticipants, other.projectParticipants)
-    && Objects.equals(publishedPapers, other.publishedPapers) && Objects.equals(topic, other.topic);
- }
-
- @Override
- public String toString() {
-  return "ResearchProject [topic=" + topic + ", publishedPapers=" + publishedPapers + ", projectParticipants="
-    + projectParticipants + "]";
- }
+    @Override
+    public String toString() {
+    	return "ResearchProject [topic=" + topic + ", publishedPapers=" + publishedPapers + ", projectParticipants="
+    			+ projectParticipants + "]";
+    }
     
 }
 
