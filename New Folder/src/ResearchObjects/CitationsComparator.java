4ae31d74e1b12347a8158f891e3bf0b766d785b5
Yerzhan Rsaliyev <rsaliyev03@gmail.com>
2023-12-13 20:08:02 +0500

    fixed spacing in the citation comparator

--- New Folder/src/ResearchObjects/CitationsComparator.java
+++ New Folder/src/ResearchObjects/CitationsComparator.java
@@ -1,9 +1,9 @@
 package ResearchObjects;
 import java.util.Comparator;
 public class CitationsComparator implements Comparator<ResearchPaper> {
- @Override
+	@Override
     public int compare(ResearchPaper paper1, ResearchPaper paper2) {
-  return Integer.compare(paper1.getCitations(), paper2.getCitations());
+		return Integer.compare(paper1.getCitations(), paper2.getCitations());
     }
 }
 
