21f6c4147f078f2b7546185ade157d43ad3862cb
Yerzhan Rsaliyev <rsaliyev03@gmail.com>
2023-12-13 20:09:43 +0500

    fixed spacing in the date comparator

--- New Folder/src/ResearchObjects/DateComparator.java
+++ New Folder/src/ResearchObjects/DateComparator.java
@@ -1,9 +1,9 @@
 package ResearchObjects;
 import java.util.Comparator;
 public class DateComparator implements Comparator<ResearchPaper> {
- @Override
+	@Override
     public int compare(ResearchPaper paper1, ResearchPaper paper2) {
-     return paper1.getDate().compareTo(paper2.getDate());
+		return paper1.getDate().compareTo(paper2.getDate());
     }
 }
 
