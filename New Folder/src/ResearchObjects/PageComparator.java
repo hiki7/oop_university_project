6ac208040456a915c560505f92d2884ff088975e
Yerzhan Rsaliyev <rsaliyev03@gmail.com>
2023-12-13 20:16:43 +0500

    added the space for @Override

--- New Folder/src/ResearchObjects/PageComparator.java
+++ New Folder/src/ResearchObjects/PageComparator.java
@@ -1,7 +1,7 @@
 package ResearchObjects;
 import java.util.Comparator;
 public class PageComparator implements Comparator<ResearchPaper> {
- @Override
+	@Override
     public int compare(ResearchPaper paper1, ResearchPaper paper2) {
         return Integer.compare(paper1.getPages(), paper2.getPages());
     }
